9f7ef761f5bfa45844907bb278442215ddf16117
Romeu Lima <romeu.sousadp@gmail.com>
2024-05-13 17:35:09 -0300

    Project Created

---
+++ src/main/java/com/softwarehouse/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.softwarehouse.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
