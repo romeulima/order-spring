2d68737ba84c8fb105f0bfd366ca01b635a9be98
Romeu Lima <romeu.sousadp@gmail.com>
2024-05-14 14:55:42 -0300

    Service layer and component registration

--- src/main/java/com/softwarehouse/order/controllers/UserController.java
+++ src/main/java/com/softwarehouse/order/controllers/UserController.java
@@ -1,18 +1,32 @@
 package com.softwarehouse.order.controllers;
 
 import com.softwarehouse.order.domain.user.User;
+import com.softwarehouse.order.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Marina", "marina@gmail.com", "99999", "romeulindo");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> userList = userService.findAll();
+        return ResponseEntity.ok().body(userList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
